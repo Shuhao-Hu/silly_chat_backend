8459d6781ee1b922a8107031194fb33e08c904ec
Shuhao Hu <hu.shuhao@outlook.com>
2025-03-31 14:26:59 +1100

    fix: add DTO for update username

---
+++ DTOs/UsernameUpdate.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace SillyChatBackend.DTOs;
+
+public class UsernameUpdate
+{
+    [Required]
+    [JsonPropertyName("username")]
+    public required string Username { get; set; }
+}
